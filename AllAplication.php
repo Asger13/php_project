92a5d1f6bb7c22da01aff278402309e7ca62373f
Asger13 <asger103@gmail.com>
2015-07-18 00:36:30 +0300

    Update AllAplication.php

--- AllAplication.php
+++ AllAplication.php
@@ -1,37 +1,47 @@
-<form action="AllAplication.php">
+<html>
+<head>
+<title>Список всех заявок</title>
+<style type="text/css">
+   h1 { 
+    font-size: 120%; 
+    font-family: Verdana, Arial, Helvetica, sans-serif; 
+    color: #333366;
+   }
+   h2{
+    font-size: 120%; 
+    font-family: Times New Roman;
+    color: blueviolet;
+    text-align: center;
+   }
+   </style>
+</head>
+<body leftmargin="30" topmargin="20">
 <?php
-define (DB_DRIVER,  "mysql");
-define (DB_CHARSET, "UTF8");
-define (DB_HOST,    "127.0.0.1");
-define (DB_USER,    "root");
-define (DB_PASS,    "");
-define (DB_NAME,    "Service");
     try 
     {
-    $db = new PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
-    
-    $stmt = $db->query('SELECT name,phone,description,file FROM sv_table ORDER BY id DESC');
-    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    require 'config.php';
+
+    $stmt = $db->prepare('SELECT name,phone,description,file FROM sv_table ORDER BY id DESC');
+    $stmt -> execute();
     
     $path ='./photodefect/';
-    echo "<table><tr>";
-    echo "\n<th>Название заявки</th>\n<th>Контактный телефон</th>\n<th>Краткое описание</th>\n<th>Изображение с деффектом</th>\n</tr>\n";
+    echo "<center><table bordercolor=red border=2 width=100%><tr>";
+    echo "\n<th><h1>Название заявки</h1></th>\n<th><h1>Контактный телефон</h1></th>\n<th><h1>Краткое описание</h1></th>\n<th><h1>Изображение с деффектом</h1></th>\n</tr>\n";
     while($row = $stmt->fetch())
     {
     $pat1=$row['file'];
     $link_img=$path.$pat1;
     echo "<tr>\n<th>" . $row['name'] . "</th>\n<th>" . $row['phone'] . "</th>\n<th>" . $row['description'] . "</th>\n<th>" ."<a href=".$link_img."><img src=".$link_img." width=70 height=50/>". "</th>\n</a></center></p>";
     }
-    echo "</table>";
+    echo "</h1></table></center><br><br>";
     }
      catch (PDOException $e)
     {
-    ?><script>alert('Сервис временно недоступен')</script><?
+    echo "Сервис временно не доступен";
     }
    
 ?>
-<a href="http://phpdevelop/XML.php">Скачать в XML</a>
+<form action="AllAplication.php">
+<h2><a href="http://phpdevelop/XML.php">Скачать в XML</a></h2>
 </form>
-
-
-
+</html>
